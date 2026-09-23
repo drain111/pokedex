a0eefd82581ad31faa82de30139ed500ade2da3d
drain111 <drain111@users.noreply.github.com>
2026-09-23 22:08:10 +0200

    Turned it into a PWA
    Added a progressive web app plugin to make the website work offline if it has been visited before and there is no internet

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,6 +12,23 @@ export default defineConfig(({ mode }) => ({
     vue(),
     vueJsx(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto', // Automatically injects the registration script into index.html      
+      devOptions: {
+        enabled: true // <--- THIS enables virtual:vite-pwa/register-sw in dev mode
+      },
+      manifest: {
+        name: 'Vue Pokedex',
+        short_name: 'Pokedex',
+        description: 'An offline-capable Pokémon application',
+        theme_color: '#ffffff',
+        
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json}']
+      }
+    }),
   ],
   base: mode === 'production' ? '/pokedex/' : '/',
   resolve: {
